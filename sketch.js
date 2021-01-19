915f7f000e97e3a7232be90720ec6bd50ab7246e
Carlos Augusto Alves Pereira <carlosaualpe@gmail.com>
2021-01-19 13:07:40 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(windowWidth, windowHeight);
 }
   
 function draw() {
-    background(220);
+    background(200);
     ellipse(50,50,80,80);
 }
